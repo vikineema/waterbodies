52efac5b5667f72f4da690d597e71ce2e3273120
vikineema <vikinyaga@gmail.com>
2024-03-22 15:18:29 +0300

    Add waterbody observations table

--- waterbodies/db_models.py
+++ waterbodies/db_models.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+
 from geoalchemy2 import Geometry
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
@@ -19,3 +22,24 @@ class WaterbodyHistoricalExtent(WaterbodyBase):
 
     def __repr__(self) -> str:
         return f"WaterbodyHistoricalExtent(uid={self.uid!r}, wb_id={self.wb_id!r}, ...)"
+
+
+class WaterbodyObservation(WaterbodyBase):
+    __tablename__ = "waterbody_observations"
+
+    obs_id: Mapped[str] = mapped_column(primary_key=True)
+    uid: Mapped[str] = mapped_column(ForeignKey("waterbodies_historical_extent.uid"), index=True)
+    px_total: Mapped[int]
+    px_wet: Mapped[float]
+    area_wet_m2: Mapped[float]
+    px_dry: Mapped[float]
+    area_dry_m2: Mapped[float]
+    px_invalid: Mapped[float]
+    area_invalid_m2: Mapped[float]
+    date: Mapped[datetime]
+
+    def __repr__(self) -> str:
+        return (
+            f"<WaterbodyObservation obs_id={self.obs_id}, uid={self.uid}, "
+            + f"date={self.date}, ...>"
+        )
