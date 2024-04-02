fef959ae261a24ca4c5015dceac61c144f36f227
vikineema <vikinyaga@gmail.com>
2024-04-02 17:32:06 +0300

    Update gridspec definition

--- waterbodies/grid.py
+++ waterbodies/grid.py
@@ -4,7 +4,7 @@ from odc.geo.gridspec import GridSpec
 
 class WaterbodiesGrid:
     def __init__(self):
-        self.grid_name = "waterbodies_grid"
-        self.grid_spec = GridSpec(
+        self.gridname = "waterbodies_grid"
+        self.gridspec = GridSpec(
             crs="EPSG:6933", tile_shape=(96000.0, 96000.0), resolution=Resolution(x=30, y=-30)
         )
