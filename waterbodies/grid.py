4ad41150828857cf205a053603da14a8ea023c6d
vikineema <vikinyaga@gmail.com>
2024-04-02 18:09:20 +0300

    Update waterbodies grid definition

--- waterbodies/grid.py
+++ waterbodies/grid.py
@@ -1,4 +1,4 @@
-from odc.geo import Resolution
+from odc.geo import XY, Resolution
 from odc.geo.gridspec import GridSpec
 
 
@@ -7,10 +7,7 @@ class WaterbodiesGrid:
         self.gridname = "waterbodies_grid"
         self.gridspec = GridSpec(
             crs="EPSG:6933",
-            tile_shape=(96000.0, 96000.0),
-            resolution=Resolution(x=30, y=-30),
-            origin=(
-                -17376000,
-                -7392000,
-            ),
+            tile_shape=XY(y=3200, x=3200),
+            resolution=Resolution(y=-30, x=30),
+            origin=XY(y=-7392000, x=-17376000),
         )
