e18e380e5b9be1bd5b6c5bcb47ace5e2ae128316
vikineema <vikinyaga@gmail.com>
2024-04-02 17:30:38 +0300

    Update gridspec definition

--- waterbodies/grid.py
+++ waterbodies/grid.py
@@ -1,6 +1,10 @@
-from odc.dscache.tools.tiling import parse_gridspec_with_name
+from odc.geo import Resolution
+from odc.geo.gridspec import GridSpec
 
 
 class WaterbodiesGrid:
     def __init__(self):
-        self.grid_name, self.gridspec = parse_gridspec_with_name("africa_30")
+        self.grid_name = "waterbodies_grid"
+        self.grid_spec = GridSpec(
+            crs="EPSG:6933", tile_shape=(96000.0, 96000.0), resolution=Resolution(x=30, y=-30)
+        )
