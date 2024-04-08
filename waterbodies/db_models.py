1411d4f9dc63a03ebd16ef55fd70734dcc12fef9
vikineema <vikinyaga@gmail.com>
2024-04-08 22:03:23 +0300

    Rename table

--- waterbodies/db_models.py
+++ waterbodies/db_models.py
@@ -23,7 +23,7 @@ class WaterbodyHistoricalExtent(WaterbodyBase):
 
 
 class WaterbodyObservation(WaterbodyBase):
-    __tablename__ = "waterbody_observations"
+    __tablename__ = "waterbody_observations_2"
 
     obs_id: Mapped[str] = Column(String, primary_key=True)
     uid: Mapped[str] = Column(String, ForeignKey("waterbodies_historical_extent.uid"), index=True)
