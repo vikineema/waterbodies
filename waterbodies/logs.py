d3e0a6f341585473fe3bf5273f7371e8614a051f
vikineema <vikinyaga@gmail.com>
2024-04-10 23:43:25 +0300

    set default log level as critical

--- waterbodies/logs.py
+++ waterbodies/logs.py
@@ -4,17 +4,18 @@ import sys
 
 def logging_setup(verbose: int = 1):
     """
-    Setup logging to print to stdout with default logging level being INFO.
+    Setup logging to print to stdout with default logging level being CRITICAL.
     """
-
-    if verbose == 0:
-        level = logging.WARNING
-    elif verbose == 1:
-        level = logging.INFO
+    if verbose == 1:
+        level = logging.CRITICAL
     elif verbose == 2:
+        level = logging.ERROR
+    elif verbose == 3:
+        level = logging.INFO
+    elif verbose == 4:
         level = logging.DEBUG
     else:
-        raise ValueError("Maximum verbosity is -vv (verbose=2)")
+        raise ValueError("Maximum verbosity is -vvvv (verbose=4)")
 
     logging.basicConfig(
         level=level,
