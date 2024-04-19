896c786e98102de84ca726220a2c671b997471a4
vikineema <vikinyaga@gmail.com>
2024-04-19 23:06:49 +0300

    Update tests

--- tests/test_surface_area_change.py
+++ tests/test_surface_area_change.py
@@ -1,12 +1,13 @@
 import json
 import logging
 import os
+import subprocess
 
 import pytest
 from click.testing import CliRunner
 
 from waterbodies.cli.surface_area_change.generate_tasks import generate_tasks
-from waterbodies.cli.surface_area_change.process_task import process_task
+from waterbodies.cli.surface_area_change.process_task import process_tasks
 
 _log = logging.getLogger(__name__)
 
@@ -42,22 +43,28 @@ def test_generate_tasks_cli_backlog_processing(
     run_type = "backlog-processing"
     temporal_range = "2016-04-05--P1D"
     historical_extent_rasters_directory = "tests/data/historical_extent_rasters_directory"
+    max_parallel_steps = 7000
     args = [
         "--verbose",
         f"--temporal-range={temporal_range}",
         f"--run-type={run_type}",
         f"--historical-extent-rasters-directory={historical_extent_rasters_directory}",
+        f"--max-parallel-steps={max_parallel_steps}",
     ]
 
     with capsys.disabled() as disabled:  # noqa F841
         result = runner.invoke(generate_tasks, args=args, catch_exceptions=True)
 
-    result_output = json.loads(result.output.split("\n")[-1])
+    assert result.exit_code == 0
 
-    assert result_output[0]["solar_day"] == expected_result[0]["solar_day"]
-    assert result_output[0]["tile_id_x"] == expected_result[0]["tile_id_x"]
-    assert result_output[0]["tile_id_y"] == expected_result[0]["tile_id_y"]
-    assert sorted(result_output[0]["task_datasets_ids"]) == sorted(
+    bash_command = "cat /tmp/tasks | jq '.[0]'"
+    task_list_str = subprocess.check_output(bash_command, shell=True, universal_newlines=True)
+    task_list = json.loads(task_list_str)
+
+    assert task_list[0]["solar_day"] == expected_result[0]["solar_day"]
+    assert task_list[0]["tile_id_x"] == expected_result[0]["tile_id_x"]
+    assert task_list[0]["tile_id_y"] == expected_result[0]["tile_id_y"]
+    assert sorted(task_list[0]["task_datasets_ids"]) == sorted(
         expected_result[0]["task_datasets_ids"]
     )
 
@@ -85,6 +92,6 @@ def test_process_tasks_cli_backlog_processing(
         "--overwrite",
     ]
     with capsys.disabled() as disabled:  # noqa F841
-        result = runner.invoke(process_task, args=args, catch_exceptions=True)
+        result = runner.invoke(process_tasks, args=args, catch_exceptions=True)
 
     _log.info(result)
