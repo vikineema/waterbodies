2ff27a4bd3ef0e066419286df47bdb721634fb72
vikineema <vikinyaga@gmail.com>
2024-04-25 22:34:20 +0300

    Add historical extent process-tasks step

--- waterbodies/cli/historical_extent/main.py
+++ waterbodies/cli/historical_extent/main.py
@@ -1,6 +1,7 @@
 import click
 
 from waterbodies.cli.historical_extent.generate_tasks import generate_tasks
+from waterbodies.cli.historical_extent.process_tasks import process_task
 from waterbodies.cli.historical_extent.rasterise_polygons import rasterise_polygons
 
 
@@ -11,3 +12,4 @@ def historical_extent():
 
 historical_extent.add_command(rasterise_polygons)
 historical_extent.add_command(generate_tasks)
+historical_extent.add_command(process_task)
