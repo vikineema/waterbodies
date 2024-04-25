988b5f72dc7948f299adc1b60908345c4215f482
vikineema <vikinyaga@gmail.com>
2024-04-25 12:15:20 +0300

    Rename tile_id to tile_index to be consistent with gridpsec

--- waterbodies/text.py
+++ waterbodies/text.py
@@ -2,69 +2,82 @@ import os
 import re
 
 
-def get_tile_id_tuple_from_str(string_: str) -> tuple[int, int]:
+def get_tile_index_tuple_from_str(string_: str) -> tuple[int, int]:
     """
-    Get the tile id (x,y) from a string.
+    Get the tile index (x,y) from a string.
 
     Parameters
     ----------
     string_ : str
-        String to search for a tile id
+        String to search for a tile index
 
     Returns
     -------
     tuple[int, int]
-        Found tile id (x,y).
+        Found tile index (x,y).
     """
-    x_id_pattern = re.compile(r"x\d{3}")
-    y_id_pattern = re.compile(r"y\d{3}")
+    x_pattern = re.compile(r"x\d{3}")
+    y_pattern = re.compile(r"y\d{3}")
 
-    tile_id_x_str = re.search(x_id_pattern, string_).group(0)
-    tile_id_y_str = re.search(y_id_pattern, string_).group(0)
+    tile_index_x_str = re.search(x_pattern, string_).group(0)
+    tile_index_y_str = re.search(y_pattern, string_).group(0)
 
-    tile_id_x = int(tile_id_x_str.lstrip("x"))
-    tile_id_y = int(tile_id_y_str.lstrip("y"))
+    tile_index_x = int(tile_index_x_str.lstrip("x"))
+    tile_index_y = int(tile_index_y_str.lstrip("y"))
 
-    tile_id = (tile_id_x, tile_id_y)
+    tile_index = (tile_index_x, tile_index_y)
 
-    return tile_id
-
-
-def get_tile_id_str_from_tuple(tile_id_tuple: tuple[int, int]) -> str:
-
-    tile_id_x, tile_id_y = tile_id_tuple
-
-    tile_id_str = f"x{tile_id_x:03d}_y{tile_id_y:03d}"
-
-    return tile_id_str
+    return tile_index
 
 
-def get_tile_id_tuple_from_filename(file_path: str) -> tuple[int, int]:
+def get_tile_index_str_from_tuple(tile_index_tuple: tuple[int, int]) -> str:
     """
-    Search for a tile id in the base name of a file.
+    Convert a tile index tuple into the tile index string format.
+
+    Parameters
+    ----------
+    tile_index_tuple : tuple[int, int]
+        Tile index tuple to convert to string.
+
+    Returns
+    -------
+    str
+        Tile index in string format.
+    """
+
+    tile_index_x, tile_index_y = tile_index_tuple
+
+    tile_index_str = f"x{tile_index_x:03d}_y{tile_index_y:03d}"
+
+    return tile_index_str
+
+
+def get_tile_index_tuple_from_filename(file_path: str) -> tuple[int, int]:
+    """
+    Search for a tile index in the base name of a file.
 
     Parameters
     ----------
     file_path : str
-        File path to search tile id in.
+        File path to search tile index in.
 
     Returns
     -------
     tuple[int, int]
-        Found tile id (x,y).
+        Found tile index (x,y).
     """
     file_name = os.path.splitext(os.path.basename(file_path))[0]
 
-    tile_id = get_tile_id_tuple_from_str(file_name)
+    tile_id = get_tile_index_tuple_from_str(file_name)
 
     return tile_id
 
 
 def get_task_id_str_from_tuple(task_id_tuple: tuple[str, int, int]) -> str:
 
-    solar_day, tile_id_x, tile_id_y = task_id_tuple
+    solar_day, tile_index_x, tile_index_y = task_id_tuple
 
-    task_id_str = f"{solar_day}/x{tile_id_x:03d}/y{tile_id_y:03d}"
+    task_id_str = f"{solar_day}/x{tile_index_x:03d}/y{tile_index_y:03d}"
 
     return task_id_str
 
@@ -78,11 +91,11 @@ def get_solar_day_from_string(string_: str) -> str:
 
 
 def get_task_id_tuple_from_str(task_id_str: str) -> tuple[str, int, int]:
-    tile_id_x, tile_id_y = get_tile_id_tuple_from_str(task_id_str)
+    tile_index_x, tile_index_y = get_tile_index_tuple_from_str(task_id_str)
 
     solar_day = get_solar_day_from_string(task_id_str)
 
-    task_id_tuple = (solar_day, tile_id_x, tile_id_y)
+    task_id_tuple = (solar_day, tile_index_x, tile_index_y)
 
     return task_id_tuple
 
@@ -108,20 +121,20 @@ def format_task(
     task_id, task_datasets_ids = next(iter(task.items()))
 
     if len(task_id) == 3:
-        solar_day, tile_id_x, tile_id_y = task_id
+        solar_day, tile_index_x, tile_index_y = task_id
 
         task = dict(
             solar_day=solar_day,
-            tile_id_x=tile_id_x,
-            tile_id_y=tile_id_y,
+            tile_index_x=tile_index_x,
+            tile_index_y=tile_index_y,
             task_datasets_ids=task_datasets_ids,
         )
     elif len(task_id) == 2:
-        tile_id_x, tile_id_y = task_id
+        tile_index_x, tile_index_y = task_id
 
         task = dict(
-            tile_id_x=tile_id_x,
-            tile_id_y=tile_id_y,
+            tile_index_x=tile_index_x,
+            tile_index_y=tile_index_y,
             task_datasets_ids=task_datasets_ids,
         )
     return task
