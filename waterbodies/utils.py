3e9a623ddecf2a3a66109679023f07d0f069896c
Victoria Neema <39700992+vikineema@users.noreply.github.com>
2024-05-14 12:46:17 +0300

    Update rio_slurp_xarray

--- waterbodies/utils.py
+++ waterbodies/utils.py
@@ -1,39 +1,29 @@
-from datacube.testutils.io import rio_slurp_read, rio_slurp_reproject
+from datacube.testutils.io import rio_slurp_xarray as dc_rio_slurp_xarray
+from datacube.utils.geometry import GeoBox as dc_Geobox
 from odc.geo.geobox import GeoBox
-from odc.geo.xr import wrap_xr
+from odc.geo.xr import wrap_xr  # noqa F401
 
 
-# Copied from
-# https://github.com/opendatacube/datacube-core/blob/9d3c6c1e63a0e269ba6d9e95482a35eda0ea4ec9/datacube/testutils/io.py#L370C1-L404C44
+# Wrapper around datacube rio_slurp_xarray
 # because of difference in behaviour between datacube.utils.geometry.Geobox
 # and odc.geo.geobox.GeoBox
 def rio_slurp_xarray(fname, *args, rgb="auto", **kw):
-    """
-    Dispatches to either:
-
-    rio_slurp_read(fname, out_shape, ..)
-    rio_slurp_reproject(fname, gbox, ...)
-
-    then wraps it all in xarray.DataArray with .crs,.nodata etc.
-    """
-
     if len(args) == 0:
         if "gbox" in kw:
-            im, mm = rio_slurp_reproject(fname, **kw)
-        else:
-            im, mm = rio_slurp_read(fname, **kw)
+            gbox = kw["gbox"]
+            if isinstance(gbox, GeoBox):
+                kw.update(
+                    {
+                        "gbox": dc_Geobox(
+                            width=gbox.width, height=gbox.height, affine=gbox.affine, crs=gbox.crs
+                        )
+                    }
+                )
     else:
         if isinstance(args[0], GeoBox):
-            im, mm = rio_slurp_reproject(fname, *args, **kw)
-        else:
-            im, mm = rio_slurp_read(fname, *args, **kw)
+            gbox = args[0]
+            args[0] = dc_Geobox(
+                width=gbox.width, height=gbox.height, affine=gbox.affine, crs=gbox.crs
+            )
 
-    if im.ndim == 3:
-        dims = ("band", *mm.gbox.dims)
-        if rgb and im.shape[0] in (3, 4):
-            im = im.transpose([1, 2, 0])
-            dims = tuple(dims[i] for i in [1, 2, 0])
-    else:
-        dims = mm.gbox.dims
-
-    return wrap_xr(im=im, gbox=mm.gbox, **dict(nodata=mm.nodata))
+    return dc_rio_slurp_xarray(fname, *args, rgb=rgb, **kw)
